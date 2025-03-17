d5030e433969347c81ff8aafba2511af4c02ceb9
SivaSurender <e.surender14@gmail.com>
2025-03-17 19:44:28 +0530

    setup

---
+++ script.js
@@ -0,0 +1,8 @@
+import "my-component";
+import "my-block";
+
+
+
+
+// Now you can use <my-component> in your HTML or React app
+document.body.innerHTML = `<my-component first="John" middle="Doe" last="Smith"></my-component><my-block></my-block>`;
